68f39276880dac28165a5c0320330bd1768b7bd4
rohankatira <113296231+rohankatira@users.noreply.github.com>
2022-12-05 12:42:55 +0530

    Add files via upload

---
+++ OOPS.cpp
@@ -0,0 +1,18 @@
+/*
+What is OOP? List OOP Concepts
+
+ What is OOP?.
+-> OOP stands for Object-Oriented_Programming.
+
+->Procedural programming is about writing procedures or Functions that perform operations on the data,
+while object-oriented-programming is about creating objects that contain both data and functions.
+
+List OOP Concept
+
+1. Class
+2. Object
+3. Inheritance
+4. Polymorphism
+5. Encapsulation
+6. Abstraction.
+*/
---
+++ diffent.cpp
@@ -0,0 +1,12 @@
+// What is the difference between OOP and POP?
+/*
+They both have different work approaches.
+On the one hand, the OOP style of programming divides a program info objects,
+whereas POP divides it info function. As there's no direct approach for a particular
+problem, a programmer uses programming languages.
+
+OOP :- Object Oriented Programming
+
+POP :- Conventional Programming, using high level language such as COBOL.
+       Fortran and Commonly know as Procedure-Oriented-Programming (POP).
+*/
